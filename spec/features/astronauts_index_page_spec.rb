1a7a56d3537adba9e7b4955f1edce1084285ed1c
Tim Allen <tnelsallen3@gmail.com>
2019-02-01 10:33:40 -0700

    Adds User Story 1

---
+++ spec/features/astronauts_index_page_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'When user goes to astronaut index page' do
+  it 'shows a list of astronauts' do
+    astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+    astronaut_2 = Astronaut.create(name: "Buzz Aldrin", age: 39, job: "Other Guy")
+
+    visit astronauts_path
+
+    expect(page).to have_content(astronaut_1.name)
+    expect(page).to have_content(astronaut_2.name)
+  end
+
+  it 'shows a name, age and job for each astronaut' do
+    astronaut_1 = Astronaut.create(name: "Neil Armstrong", age: 37, job: "Commander")
+    astronaut_2 = Astronaut.create(name: "Buzz Aldrin", age: 39, job: "Other Guy")
+
+    visit astronauts_path
+
+    within "#astronaut-#{astronaut_1.id}" do
+      expect(page).to have_content(astronaut_1.name)
+      expect(page).to have_content(astronaut_1.age)
+      expect(page).to have_content(astronaut_1.job)
+    end
+
+    within "#astronaut-#{astronaut_2.id}" do
+      expect(page).to have_content(astronaut_2.name)
+      expect(page).to have_content(astronaut_2.age)
+      expect(page).to have_content(astronaut_2.job)
+    end
+  end
+
+  describe 'When I look in the stats section' do
+    it 'shows the average age of all astronauts' do
+    end
+  end
+end
